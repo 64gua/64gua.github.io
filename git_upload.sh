66099dbcb82fde4b88bccb0a5234fbf3e6c35e94
Robert Wang <wangyuhui72@163.com>
2025-03-06 11:50:30 +0800

    kui2dayou

--- git_upload.sh
+++ git_upload.sh
@@ -1,2 +1,2 @@
-git config --global http.proxy http://127.0.0.1:54408
-git config --global https.proxy https://127.0.0.1:54408
+git config --global http.proxy http://127.0.0.1:64934
+git config --global https.proxy https://127.0.0.1:64934
